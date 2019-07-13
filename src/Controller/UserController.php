4d584bc9f709e97b8f07dbf45839ab98e1c6d0d4
AnaiO <anais.berton.io@gmail.com>
2019-07-13 16:29:21 +0200

    signUp method and singuptype done

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,41 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\SignUpType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/signup"), name="sign_up", methods={"GET", "POST"}
      */
-    // public function signUp()
-    // {
-    //     $user = new User;
-    //     $form = $this->createForm()
-    // }
+    public function signUp(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User;
+        $form = $this->createForm(SignUpType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            
+
+            $encodedPassword = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($encodedPassword);
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('question_list');
+        }
+
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
 }
--- src/Form/SignUpType.php
+++ src/Form/SignUpType.php
@@ -5,21 +5,32 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class SignUpType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('password')
-            ->add('image')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('role')
+            ->add('firstname', TextType::class, [
+                'constraints' => new NotBlank()
+            ])
+            ->add('lastname', TextType::class, [
+                'constraints' => new NotBlank()
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => new NotBlank
+            ])
+            ->add('password', PasswordType::class)
+            ->add('image', FileType::class, [
+                'label' => 'jpg, png, gif'
+            ])
+
         ;
     }
 
@@ -27,6 +38,7 @@ class SignUpType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'attr' => ['novalidate' => 'novalidate']
         ]);
     }
 }
