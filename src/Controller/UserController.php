1182a7f7873170cb321070087b37b869dcbfe3e8
AnaiO <anais.berton.io@gmail.com>
2019-07-13 15:49:42 +0200

    SignUp form + usercontroller signup method on process

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/signup"), name="sign_up", methods={"GET", "POST"}
+     */
+    // public function signUp()
+    // {
+    //     $user = new User;
+    //     $form = $this->createForm()
+    // }
+}
