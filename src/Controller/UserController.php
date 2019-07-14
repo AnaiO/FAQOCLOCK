88d1ecd89d53ac2f65451656391ffb3c38966c6e
AnaiO <anais.berton.io@gmail.com>
2019-07-14 18:23:35 +0200

    add profile + profile edit ok both

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\SignUpType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -12,7 +13,52 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
-   //Profile /user/profile  show   profile_show
+    /**
+     * @Route("/user/profile", name="profile_show", methods={"GET"})
+     */
+    public function show()
+    {
+        return $this->render('user/profile.html.twig');
+    }
+
+    /**
+     * @Route("/user/profile/edit", name="profile_edit", methods={"GET","POST"})
+     */
+    public function edit(Security $security, UserPasswordEncoderInterface $passwordEncoder, Request $request)
+    {
+        $user = $security->getUser();
+       
+        $form = $this->createForm(SignUpType::class, $user);
+
+        $oldPassword = $user->getPassword();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!is_null($user->getPassword())) {
+                $encodedPassword = $passwordEncoder->encodePassword(
+                    $user,
+                    $user->getPassword()
+               );
+            } else {
+                $encodedPassword = $oldPassword;
+            }
+
+            $user->setPassword($encodedPassword);
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('profile_show');
+        }
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    
+
+
 
    //edit profile /user/profile/edit   edit profile_edit
 
--- src/Form/SignUpType.php
+++ src/Form/SignUpType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -11,11 +13,45 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class SignUpType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $listener = function (FormEvent $event){
+            $user = $event->getData();
+            $form = $event->getForm();
+
+            if(is_null($user->getId())){ // si notre id d'objet est null == creation de user
+                $form->add('password', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'The password fields must match.',
+                    'options' => ['attr' => ['class' => 'password-field']],
+                    'required' => true,
+                    'first_options'  => ['label' => 'Password'],
+                    'second_options' => ['label' => 'Repeat Password'],
+                    'constraints' => [
+                        new NotBlank()
+                    ]
+                ]);
+            } else { //sinon je suis en mise a jour car l'id est present
+                $form->add('password', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'The password fields must match.',
+                    'options' => ['attr' => ['class' => 'password-field']],
+                    'required' => true,
+                    'first_options'  => [
+                        'label' => 'Password',
+                        'attr' => [
+                            'placeholder' => 'Laisser vide si inchangé'
+                        ]
+                    ],
+                    'second_options' => ['label' => 'Repeat Password'],
+                ]);
+            }
+        };
+        
         $builder
             ->add('firstname', TextType::class, [
                 'constraints' => new NotBlank()
@@ -26,10 +62,7 @@ class SignUpType extends AbstractType
             ->add('email', EmailType::class, [
                 'constraints' => new NotBlank
             ])
-            ->add('password', PasswordType::class)
-            ->add('image', FileType::class, [
-                'label' => 'jpg, png, gif'
-            ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, $listener)
 
         ;
     }
