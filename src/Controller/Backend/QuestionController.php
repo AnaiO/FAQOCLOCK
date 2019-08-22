b66883ad45d11e107e9e52068ce469ee5af63d65
AnaiO <anais.berton.io@gmail.com>
2019-08-22 17:55:50 +0200

    fix route name for security access

--- src/Controller/Backend/QuestionController.php
+++ src/Controller/Backend/QuestionController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/Backend/question/{id}/toggle", name="backend_question_toggle", methods={"GET", "POST"})
+     * @Route("/backend/question/{id}/toggle", name="backend_question_toggle", methods={"GET", "POST"})
      */
     public function toggle(Question $question, ObjectManager $om)
     {
